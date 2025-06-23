66009a661a0d2a7ab647454b9c40fa23fa508aa7
Guilherme "Grimm" Rodrigues <guilher.c.rodrigues@gmail.com>
2025-06-23 19:43:03 -0300

    Add files via upload

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,58 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = true,
+  },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+      toggle_hidden_files = true,
+    },
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    event = 'BufReadPre',
+    opts = {
+      mode = 'cursor',
+      max_lines = 3,
+    },
+  },
+  { 'sainnhe/gruvbox-material' },
+  {
+    'Exafunction/windsurf.vim',
+    vim.api.nvim_set_var('codeium_server_config', {
+      portal_url = 'https://codeium.dell.com',
+      api_url = 'https://codeium.dell.com/_route/api_server',
+    }),
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<C-g>', function()
+        return vim.fn['codeium#Accept']()
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-;>', function()
+        return vim.fn['codeium#CycleCompletions'](1)
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-,>', function()
+        return vim.fn['codeium#CycleCompletions'](-1)
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-x>', function()
+        return vim.fn['codeium#Clear']()
+      end, { expr = true, silent = true })
+    end,
+  },
+}
